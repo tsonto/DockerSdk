4a3fc57c6f9f396454e71c95016d760dfb14ad93
Matt Morrison <3241034+Emdot@users.noreply.github.com>
2021-03-26 23:02:48 -0500

    Initial commit

---
+++ DockerSdk/DaemonNotFoundException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DockerSdk
+{
+    /// <summary>
+    /// Indicates that the Docker daemon is not present or not running.
+    /// </summary>
+    [Serializable]
+    public class DaemonNotFoundException : DockerException
+    {
+        /// <summary>
+        /// </summary>
+        public DaemonNotFoundException() { }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="message"></param>
+        public DaemonNotFoundException(string message) : base(message) { }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public DaemonNotFoundException(string message, Exception inner) : base(message, inner) { }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected DaemonNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
