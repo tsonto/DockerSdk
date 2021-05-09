e0830de38e9f088781f8c88d1d136ae955a00673
Matt Tsōnto <matt@tsonto.com>
2021-05-08 20:33:09 -0500

    Added NetworkAccess.ListAsync method. (#16)
    * Added NetworkAccess.ListAsync method. Also adjusted a container test for some odd Docker behavior.
    * Replaced the overlay network in tests with the none and host networks, to not require a swarm.

--- DockerSdk.Tests/NetworkAccessTests.cs
+++ DockerSdk.Tests/NetworkAccessTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using DockerSdk.Containers;
@@ -44,7 +45,7 @@ namespace DockerSdk.Tests
             using var client = await DockerClient.StartAsync();
             using var cli = new DockerCli(toh);
 
-            // Create a network for testing. To avoid errors where Docker runs out of network
+            // Create a network for testing.
             var name = "ddnt-" + nameof(GetAsync_ByName_NetworkExists_Success);
             string id = cli.Invoke($"network create --subnet 201.101.103.0/24 {name}")[0];  // subnet is to avoid errors from running out of IPs from the default address pool
 
@@ -69,42 +70,6 @@ namespace DockerSdk.Tests
 
             await Assert.ThrowsAsync<NetworkNotFoundException>(
                 () => client.Networks.GetAsync(name));
-        }
-
-        [Fact]
-        public async Task GetDetailsAsync_NetworkIsAttachedToContainer_GetsEndpointDetails()
-        {
-            using var client = await DockerClient.StartAsync();
-            using var cli = new DockerCli(toh);
-            string networkId = cli.GetNetworkId("general");
-
-            // Set up a container and connect to it.
-            string containerNameString = "ddnt-" + nameof(GetDetailsAsync_NetworkIsAttachedToContainer_GetsEndpointDetails);
-            string containerIdString = cli.Invoke($"container run --rm --detach --name {containerNameString} ddnt:infinite-loop")[0];
-            cli.Invoke($"network connect --ip 12.34.56.71 --ip6 1234:5678:0000:0000:0000:ff00:0042:8329 {networkId} {containerIdString}");
-
-            ContainerName containerNameObject = ContainerName.Parse(containerNameString);
-            try
-            {
-                INetworkInfo network = await client.Networks.GetInfoAsync(networkId);
-
-                network.AttachedContainers.Should().HaveCount(1);   
-                network.Endpoints.Should().HaveCount(1);
-                network.EndpointsByContainerName.Should().HaveCount(1);
-                network.EndpointsByContainerName.Keys.Should().Contain(containerNameObject);
-
-                var endpoint = network.EndpointsByContainerName[containerNameObject];
-                endpoint.Container.Id.ToString().Should().Be(containerIdString);
-                endpoint.Id.Should().NotBeNullOrWhiteSpace();
-                endpoint.IPv4Address?.ToString().Should().Be("12.34.56.71");
-                endpoint.IPv6Address.Should().Be(IPAddress.Parse("1234:5678::ff00:42:8329"));
-                endpoint.MacAddress.Should().NotBeNull();
-                endpoint.Network.Id.ToString().Should().Be(networkId);
-            }
-            finally
-            {
-                cli.Invoke($"container rm --force {containerIdString}", ignoreErrors: true);
-            }
         }
 
         [Fact]
@@ -139,6 +104,112 @@ namespace DockerSdk.Tests
             network.Pools[1].Range.Should().BeNull();
             network.Pools[1].Subnet.ToString().Should().Be("1234:5678::/32");
             network.Scope.Should().Be(NetworkScope.Local);
+        }
+
+        [Fact]
+        public async Task GetDetailsAsync_NetworkIsAttachedToContainer_GetsEndpointDetails()
+        {
+            using var client = await DockerClient.StartAsync();
+            using var cli = new DockerCli(toh);
+            string networkId = cli.GetNetworkId("general");
+
+            // Set up a container and connect to it.
+            string containerNameString = "ddnt-" + nameof(GetDetailsAsync_NetworkIsAttachedToContainer_GetsEndpointDetails);
+            string containerIdString = cli.Invoke($"container run --rm --detach --name {containerNameString} ddnt:infinite-loop")[0];
+            cli.Invoke($"network connect --ip 12.34.56.71 --ip6 1234:5678:0000:0000:0000:ff00:0042:8329 {networkId} {containerIdString}");
+
+            ContainerName containerNameObject = ContainerName.Parse(containerNameString);
+            try
+            {
+                INetworkInfo network = await client.Networks.GetInfoAsync(networkId);
+
+                network.AttachedContainers.Should().HaveCount(1);
+                network.Endpoints.Should().HaveCount(1);
+                network.EndpointsByContainerName.Should().HaveCount(1);
+                network.EndpointsByContainerName.Keys.Should().Contain(containerNameObject);
+
+                var endpoint = network.EndpointsByContainerName[containerNameObject];
+                endpoint.Container.Id.ToString().Should().Be(containerIdString);
+                endpoint.Id.Should().NotBeNullOrWhiteSpace();
+                endpoint.IPv4Address?.ToString().Should().Be("12.34.56.71");
+                endpoint.IPv6Address.Should().Be(IPAddress.Parse("1234:5678::ff00:42:8329"));
+                endpoint.MacAddress.Should().NotBeNull();
+                endpoint.Network.Id.ToString().Should().Be(networkId);
+            }
+            finally
+            {
+                cli.Invoke($"container rm --force {containerIdString}", ignoreErrors: true);
+            }
+        }
+
+        [Fact]
+        public async Task ListAsync_FilterByDriver_Success()
+        {
+            using var client = await DockerClient.StartAsync();
+            using var cli = new DockerCli(toh);
+            var generalNetworkId = cli.GetNetworkId("general");
+            var hostNetworkId = cli.GetHostNetworkId();
+
+            var options = new ListNetworksOptions
+            {
+                DriverFilter = new[] { "host" },
+            };
+            var actual = await client.Networks.ListAsync(options);
+
+            actual.Select(net => net.Id.ToString()).Should().NotContain(generalNetworkId);
+            actual.Select(net => net.Id.ToString()).Should().Contain(hostNetworkId);
+        }
+
+        [Fact]
+        public async Task ListAsync_FilterById_Success()
+        {
+            using var client = await DockerClient.StartAsync();
+            using var cli = new DockerCli(toh);
+            var generalNetworkId = cli.GetNetworkId("general");
+            var hostNetworkId = cli.GetHostNetworkId();
+
+            var options = new ListNetworksOptions
+            {
+                IdFilter = new[] { generalNetworkId[1..^2] },  // this assumes that the IDs of the two networks differ by more than the first and last digits, which is extremely likely
+            };
+            var actual = await client.Networks.ListAsync(options);
+
+            actual.Select(net => net.Id.ToString()).Should().Contain(generalNetworkId);
+            actual.Select(net => net.Id.ToString()).Should().NotContain(hostNetworkId);
+        }
+
+        [Fact]
+        public async Task ListAsync_FilterByName_Success()
+        {
+            using var client = await DockerClient.StartAsync();
+            using var cli = new DockerCli(toh);
+            var generalNetworkId = cli.GetNetworkId("general");
+            var noneNetworkId = cli.GetNoneNetworkId();
+
+            var options = new ListNetworksOptions
+            {
+                NameFilter = new[] { "-gener" },
+            };
+            var actual = await client.Networks.ListAsync(options);
+
+            actual.Select(net => net.Id.ToString()).Should().Contain(generalNetworkId);
+            actual.Select(net => net.Id.ToString()).Should().NotContain(noneNetworkId);
+        }
+
+        [Fact]
+        public async Task ListAsync_Success()
+        {
+            using var client = await DockerClient.StartAsync();
+            using var cli = new DockerCli(toh);
+            var generalNetworkId = cli.GetNetworkId("general");
+            var noneNetworkId = cli.GetNoneNetworkId();
+            var hostNetworkId = cli.GetHostNetworkId();
+
+            var actual = await client.Networks.ListAsync();
+
+            actual.Select(net => net.Id.ToString()).Should().Contain(generalNetworkId);
+            actual.Select(net => net.Id.ToString()).Should().Contain(noneNetworkId);
+            actual.Select(net => net.Id.ToString()).Should().Contain(hostNetworkId);
         }
     }
 }
--- DockerSdk/Networks/NetworkAccess.cs
+++ DockerSdk/Networks/NetworkAccess.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Core = Docker.DotNet;
+using CoreModels = Docker.DotNet.Models;
 
 namespace DockerSdk.Networks
 {
@@ -82,6 +87,132 @@ namespace DockerSdk.Networks
                 throw new ArgumentNullException(nameof(network));
 
             return await NetworkFactory.LoadInfoAsync(client, network, ct).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Gets a list of Docker networks known to the daemon.
+        /// </summary>
+        /// <param name="ct">A token used to cancel the operation.</param>
+        /// <returns>A <see cref="Task{TResult}"/> that resolves to the list of images.</returns>
+        /// <remarks>The sequence of the results is undefined.</remarks>
+        /// <exception cref="System.Net.Http.HttpRequestException">
+        /// The request failed due to an underlying issue such as network connectivity.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">An input is null.</exception>
+        public Task<IReadOnlyList<INetwork>> ListAsync(CancellationToken ct = default)
+            => ListAsync(new(), ct);
+
+        /// <summary>
+        /// Gets a list of Docker networks known to the daemon.
+        /// </summary>
+        /// <param name="options">Filters for the search.</param>
+        /// <param name="ct">A token used to cancel the operation.</param>
+        /// <returns>A <see cref="Task{TResult}"/> that resolves to the list of images.</returns>
+        /// <remarks>The sequence of the results is undefined.</remarks>
+        /// <exception cref="System.Net.Http.HttpRequestException">
+        /// The request failed due to an underlying issue such as network connectivity.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">An input is null.</exception>
+        public async Task<IReadOnlyList<INetwork>> ListAsync(ListNetworksOptions options, CancellationToken ct = default)
+        {
+            // Validate the input.
+            if (options is null)
+                throw new ArgumentNullException(nameof(options));
+
+            // Construct the API request.
+            var request = new CoreModels.NetworksListParameters
+            {
+                Filters = MakeListFilters(options, out bool none),
+            };
+            if (none)
+                return Array.Empty<INetwork>();
+
+            // Send the API request.
+            IList<CoreModels.NetworkResponse> response;
+            try
+            {
+                response = await client.Core.Networks.ListNetworksAsync(request, ct).ConfigureAwait(false);
+            }
+            catch (Core.DockerApiException ex)
+            {
+                throw DockerException.Wrap(ex);
+            }
+
+            // Convert the response into network objects.
+            return response
+                .Select(net => new Network(client, new NetworkFullId(net.ID)))
+                .ToArray();
+        }
+
+        private static IDictionary<string, IDictionary<string, bool>>? MakeListFilters(ListNetworksOptions options, out bool none)
+        {
+            var output = new Dictionary<string, IDictionary<string, bool>>();
+            none = false;
+
+            if (options.DanglingNetworksFilter == true)
+                output.Add("dangling", new Dictionary<string, bool> { ["true"] = true });
+            else if (options.DanglingNetworksFilter == false)
+                output.Add("dangling", new Dictionary<string, bool> { ["false"] = true });
+
+            if (options.BuiltInNetworksFilter == true)
+                output.Add("type", new Dictionary<string, bool> { ["builtin"] = true });
+            else if (options.BuiltInNetworksFilter == false)
+                output.Add("type", new Dictionary<string, bool> { ["custom"] = true });
+
+            if (options.ScopeFilter is not null)
+            {
+                if (options.ScopeFilter == NetworkScope.Local)
+                    output.Add("scope", new Dictionary<string, bool> { ["local"] = true });
+                else if (options.ScopeFilter == NetworkScope.Swarm)
+                    output.Add("scope", new Dictionary<string, bool> { ["swarm"] = true });
+                else
+                    throw new NotImplementedException($"Scope type {options.ScopeFilter} is not supported for filtering networks.");
+            }
+
+            var labelsDictionary = new Dictionary<string, bool>();
+            foreach (var label in options.LabelExistsFilters)
+                labelsDictionary.Add(label, true);
+            foreach (var (label, value) in options.LabelValueFilters)
+                labelsDictionary.Add($"{label}={value}", true);
+            if (labelsDictionary.Any())
+                output.Add("label", labelsDictionary);
+
+            if (options.DriverFilter is not null)
+            {
+                var driversDict = new Dictionary<string, bool>();
+                foreach (var driverName in options.DriverFilter)
+                    driversDict.Add(driverName, true);
+                if (driversDict.Any())
+                    output.Add("driver", driversDict);
+                else
+                    none = true;
+            }
+
+            if (options.IdFilter is not null)
+            {
+                var idsDict = new Dictionary<string, bool>();
+                foreach (var id in options.IdFilter)
+                    idsDict.Add(id, true);
+                if (idsDict.Any())
+                    output.Add("id", idsDict);
+                else
+                    none = true;
+            }
+
+            if (options.NameFilter is not null)
+            {
+                var idsDict = new Dictionary<string, bool>();
+                foreach (var name in options.NameFilter)
+                    idsDict.Add(name, true);
+                if (idsDict.Any())
+                    output.Add("name", idsDict);
+                else
+                    none = true;
+            }
+
+            if (!output.Any())
+                return null;
+            return output;
         }
     }
 }
